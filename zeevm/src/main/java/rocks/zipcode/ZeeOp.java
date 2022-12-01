a34c08ab3cbc75605bcf6d2cefa30ee234171550
Tarin <tarin@Zipcoders-MacBook-Pro-5.local>
2022-11-30 21:01:18 -0500

    completed

--- zeevm/src/main/java/rocks/zipcode/ZeeOp.java
+++ zeevm/src/main/java/rocks/zipcode/ZeeOp.java
@@ -46,6 +46,24 @@ public enum ZeeOp {
             operandStack.push(operandStack.pop() - second);
         }
     },
+    MULT("multiply"){
+        public void execute(String[] args) {
+            int second = operandStack.pop();
+            operandStack.push(operandStack.pop() * second);
+        }
+    },
+    DIVIDE("divide"){
+        public void execute(String[] args) {
+            int second = operandStack.pop();
+            operandStack.push(operandStack.pop() / second);
+        }
+    },
+    MOD("modulus"){
+        public void execute(String[] args) {
+            int second = operandStack.pop();
+            operandStack.push(operandStack.pop() % second);
+        }
+    },
     PRINT("print"){
         public void execute(String[] args) {
                 String p = operandStack.pop().toString();
